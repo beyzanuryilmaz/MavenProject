db1d3fd2deca77d856e834bda33c4522e543f939
beyzanurozdemir <beyzanurozdemir96@gmail.com>
2022-04-15 16:20:43 +0200

    added more hello feature

--- src/test/java/HelloTest.java
+++ src/test/java/HelloTest.java
@@ -2,5 +2,7 @@ public class HelloTest {
     public static void main(String[] args) {
         System.out.println("Hello test");
         System.out.println("Developing hello feature");
+        System.out.println("Local repo is already published to Github");
+        System.out.println("Doing more work");
     }
 }
